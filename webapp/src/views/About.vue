8132cdb2fb0661a3a985bafcc4a039fa9b9841ac
gomez-addams <gomez-addams@users.noreply.github.com>
2019-01-17 13:30:44 -0700

    First version of the webapp implementation

---
+++ webapp/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About this webapp</h1>
+    <p>This app is part of a programming challenge using Kotlin for a backend and vue.js for a frontend
+    to access tmdb.org and search for movies based on titles.</p>
+    <p>Be sure you've started the local webserver before using this webapp.</p>
+  </div>
+</template>
---
+++ webapp/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <MovieVue msg="Welcome to the MovieVue webapp"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MovieVue from '@/components/MovieVue.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MovieVue
+  }
+}
+</script>
